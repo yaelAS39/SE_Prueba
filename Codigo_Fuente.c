7a9d77e1cefa29b0c15f1d2ff52fbeb0901d69d0
yaelAS39 <89820281+yaelAS39@users.noreply.github.com>
2021-09-03 12:53:40 -0500

    Add files via upload

---
+++ Codigo_Fuente.c
@@ -0,0 +1,14 @@
+////////////////////////////////////////
+// Codigo Fuente
+//
+// Iniciar un repositorio en Git
+//
+// Yael Alvarado Santillán
+// 3 de septiembre de 2021
+//
+////////////////////////////////////////
+
+// void main
+int main() {
+    printf("Primer repositorio en GitHub para Sistemas Embebidos");
+}
